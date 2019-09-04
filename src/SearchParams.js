5d7b1b2db9fe76bba37a54d1a8b8529d4bf30b8e
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-09-04 12:58:28 -0700

    Add files via upload

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, {
+    useState
+} from 'react';
 
 const SearchParams = () => {
-    const location = "San Francisco, CA";
+    const [location, setLocation] = useState("San Francisco, CA");
 
     return ( <
         div className = "search-params" >
         <
+        h1 > {
+            location
+        } < /h1>  <
         form >
         <
         label htmlFor = "location" >
-        Location <
-        input id = "location"
+        Location < input id = "location"
         value = {
             location
         }
-        placeholder = "Location" / >
-
-        <
-        /label> <
-        button > Submit < /button> < /
+        placeholder = "Location"
+        onChange = {
+            e => setLocation(e.target.value)
+        }
+        />  < /
+        label > <
+        button > Submit < /button>   < /
         form >
 
         <
