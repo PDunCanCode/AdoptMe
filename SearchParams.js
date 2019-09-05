e7a38c99600bcf863a631b5213dac650b839a0c6
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-09-04 19:46:22 -0700

    Add files via upload

---
+++ SearchParams.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("San Francisco, CA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+      setBreeds([]);
+      setBreed("");
+
+      pet.breeds(animal).then(({ breeds }) => {
+        const breedStrings = breeds.map(({ name }) => name);
+        setBreeds(breedStrings);
+      }, console.error); 
+      
+  }, [animal, setBreed, setBreeds]);
+
+  return (
+    <div className="search-params">
+      <h1> {location} </h1>
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
+          <button> Submit </button>
+        </label>
+      </form>
+    </div>
+  );
+};
